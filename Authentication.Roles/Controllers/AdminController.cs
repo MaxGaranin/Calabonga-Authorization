1ddbbc882cdb90925b8242fe3a729d1a05c85261
Max Garanin <maxgaranin@gmail.com>
2020-06-26 13:54:20 +0400

    2 Аутентификация: Управление ролями и разрешениями

---
+++ Authentication.Roles/Controllers/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.Roles.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "Administrator")]
+        public IActionResult Administrator()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "Manager")]
+        public IActionResult Manager()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, model.UserName),
+                new Claim(ClaimTypes.Role, "Administrator"),
+            };
+            var claimsIdentity = new ClaimsIdentity(claims, "Cookie");
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            await HttpContext.SignInAsync("Cookie", claimsPrincipal);
+
+            return Redirect(model.ReturnUrl);
+        }
+
+        public IActionResult LogOff()
+        {
+            HttpContext.SignOutAsync("Cookie");
+            return Redirect("/Home/Index");
+        }
+    }
+}
