7eba8cc5af47bd864860fed08fa6df8f84b2f196
Max Garanin <maxgaranin@gmail.com>
2020-07-15 00:20:00 +0400

    4 Facebook provider

---
+++ Authorization.FacebookDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Authorization.FacebookDemo.Data.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authorization.FacebookDemo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
