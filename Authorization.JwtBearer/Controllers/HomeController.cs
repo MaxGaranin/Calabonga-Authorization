0843f88b5c1df13c422e7aa6b297d7789b7ecd40
Max Garanin <maxgaranin@gmail.com>
2020-07-17 13:36:35 +0400

    5 JWT Bearer

---
+++ Authorization.JwtBearer/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Authorization.JwtBearer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "Arcadiy"),
+                new Claim(JwtRegisteredClaimNames.Email, "arc@mail.ru"),
+            };
+
+            byte[] secretBytes = Encoding.UTF8.GetBytes(Constants.SecretKey);
+
+            var key = new SymmetricSecurityKey(secretBytes);
+
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                Constants.Issuer,
+                Constants.Audience,
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials);
+
+            var value = new JwtSecurityTokenHandler().WriteToken(token);
+
+            ViewBag.Token = value;
+            return View();
+        }
+    }
+}
